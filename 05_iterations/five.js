dde434ab140e1898c0cfab022f6caee4d69be4a4
AFRIDE <85386619+afride596@users.noreply.github.com>
2024-08-28 13:47:13 +0530

    f

--- 05_iterations/five.js
+++ 05_iterations/five.js
@@ -7,7 +7,6 @@ const coding = ["js", "ruby", "java", "python", "cpp"]
 // coding.forEach( (item) => {
 //     console.log(item);
 // } )
-gfdg
 // function printMe(item){
 //     console.log(item);
 // }
